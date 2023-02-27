33fa38e3d3eafa69ec401fd63dbc49b57fc3317e
jangir-ritik <115213651+jangir-ritik@users.noreply.github.com>
2023-02-27 12:39:41 +0530

    MAJOR: added webpack

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const bodyParser = require('body-parser');
-const validateIssue = require('./issue')
+const { validateIssue } = require('./issue')
 
 const app = express();
 app.use(express.static('static'))
