0c7d1a872705d465df62f74aa137502b56a81395
jangir-ritik <115213651+jangir-ritik@users.noreply.github.com>
2023-03-02 14:42:31 +0530

    MAJOR: until react router

--- server/issue.js
+++ server/issue.js
@@ -1,35 +1,35 @@
 const validIssueStatus = {
-    New: true,
-    Open: true,
-    Assigned: true,
-    Fixed: true,
-    Verified: true,
-    Closed: true,
-  };
+  New: true,
+  Open: true,
+  Assigned: true,
+  Fixed: true,
+  Verified: true,
+  Closed: true,
+};
 
-  const issueFieldType = {
-    id: 'optional',
-    status: 'required',
-    owner: 'required',
-    effort: 'optional',
-    created: 'required',
-    completionDate: 'optional',
-    title: 'required',
-  };
+const issueFieldType = {
+  id: 'optional',
+  status: 'required',
+  owner: 'required',
+  effort: 'optional',
+  created: 'required',
+  completionDate: 'optional',
+  title: 'required',
+};
 
-  function validateIssue(issue) {
-    for (const field in issueFieldType) {
-      const type = issueFieldType[field];
-      if (!type) {
-        delete issue[field];
-      } else if (type === 'required' && !issue[field]) {
-        return `${field} is required.`;
-  } }
-    if (!validIssueStatus[issue.status])
-      return `${issue.status} is not a valid status.`;
-    return null;
+function validateIssue(issue) {
+  for (const field in issueFieldType) {
+    const type = issueFieldType[field];
+    if (!type) {
+      delete issue[field];
+    } else if (type === 'required' && !issue[field]) {
+      return `${field} is required.`;
+    }
   }
+  if (!validIssueStatus[issue.status]) { return `${issue.status} is not a valid status.`; }
+  return null;
+}
 
 export default {
-    validateIssue: validateIssue
-}
+  validateIssue,
+};
